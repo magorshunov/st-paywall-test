4826d0cefa2cd3573a88bae3b53adb53b5c75b5a
Mikhail Gorshunov <magorshunov@gmail.com>
2024-09-28 14:52:57 -0700

    added notes

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from st_paywall import add_auth
 add_auth(required=True)
 
 
+st.write(st.session_state.email)
+st.write(st.session_state.user_subscribed)
 
 with st.chat_message("assistant"):
     st.write("Hello human")
